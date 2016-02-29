737439dc3ab704db27bda3096149b52dc8185b19
Andy Fang <andyfang.dz@gmail.com>
2016-02-29 11:45:48 -0500

    More Detail

--- MyFirstApp/app/src/main/java/com/example/wenqixian/myfirstapp/activities/MovieDetailActivity.java
+++ MyFirstApp/app/src/main/java/com/example/wenqixian/myfirstapp/activities/MovieDetailActivity.java
@@ -6,9 +6,19 @@ import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import com.example.wenqixian.myfirstapp.R;
 import com.example.wenqixian.myfirstapp.models.Movie;
+import com.example.wenqixian.myfirstapp.models.MovieRating;
+import com.example.wenqixian.myfirstapp.singletons.FirebaseSingleton;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
 
 public class MovieDetailActivity extends Activity {
 
@@ -43,6 +53,31 @@ public class MovieDetailActivity extends Activity {
             TextView view = (TextView) findViewById(R.id.movie_title);
             view.setText(movieName);
         }
+        Firebase myFirebaseRef = FirebaseSingleton.getInstance().ref();
+        Firebase comment = myFirebaseRef.child("comments").child(myFirebaseRef.getAuth().getUid()
+                + '_' + mMovie.getId());
+        comment.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
+                MovieRating rating = snapshot.getValue(MovieRating.class);
+                if (rating != null) {
+                    TextView commentText = ((TextView) findViewById(R.id.rating_content));
+                    commentText.setText(rating.getComment());
+                    RatingBar ratingbar = ((RatingBar) findViewById(R.id.ratingBar2));
+                    ratingbar.setRating(rating.getScore());
+                }
+                ProgressBar pb = (ProgressBar)findViewById(R.id.progressBar);
+                pb.setVisibility(View.GONE);
+                RelativeLayout rl = (RelativeLayout) findViewById(R.id.rating_area);
+                rl.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                System.out.println("The read failed: " + firebaseError.getMessage());
+            }
+        });
 
     }
 
