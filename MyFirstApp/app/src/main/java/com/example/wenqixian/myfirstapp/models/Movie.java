5a7dd3698e3cbebc3b2b47aa56f1e0b98369e437
LirenFish <Monna@lawn-128-61-47-26.lawn.gatech.edu>
2016-04-05 22:43:37 -0400

    MovieClassTest

--- MyFirstApp/app/src/main/java/com/example/wenqixian/myfirstapp/models/Movie.java
+++ MyFirstApp/app/src/main/java/com/example/wenqixian/myfirstapp/models/Movie.java
@@ -4,29 +4,34 @@ package com.example.wenqixian.myfirstapp.models;
  * Created by andy on 2/19/16.
  */
 public class Movie {
+
+    private String id;
+    private String name;
+
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
+        if(id==null) {
+            throw new IllegalArgumentException("gtid cannot be null");
+        }
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
+        if(name==null) {
+            throw new IllegalArgumentException("gtid cannot be null");
+        }
         this.name = name;
     }
-
     public Movie(String id, String name) {
         this.id = id;
         this.name = name;
     }
-
-    private String id;
-    private String name;
 
     public static Movie getMovieById() {
         return null;
